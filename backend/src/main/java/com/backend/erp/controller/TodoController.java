6f902bd4b969618f8c52d42d36b26dbe3f370f83
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-03-19 21:05:58 +0530

    delete and fetch request added

--- backend/src/main/java/com/backend/erp/controller/TodoController.java
+++ backend/src/main/java/com/backend/erp/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.backend.erp.controller;
 
 import com.backend.erp.request.TodoRequest;
 import com.backend.erp.response.SuccessResponse;
+import com.backend.erp.response.TodoResponse;
 import com.backend.erp.service.TodoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,19 @@ public class TodoController {
     @Autowired
     private TodoServiceImpl service;
 
+    @GetMapping("/")
+    public ResponseEntity<TodoResponse> fetchTodos(@RequestHeader(value = "Authorization") String authToken) {
+        return ResponseEntity.ok(service.fetch(authToken));
+    }
+
     @PostMapping("/add")
-    public ResponseEntity<SuccessResponse> register(@RequestBody TodoRequest request,@RequestHeader(value="Authorization") String authToken) {
-        return ResponseEntity.ok(service.add(request,authToken));
+    public ResponseEntity<SuccessResponse> addTodo(@RequestBody TodoRequest request, @RequestHeader(value = "Authorization") String authToken) {
+        return ResponseEntity.ok(service.add(request, authToken));
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<SuccessResponse> deleteTodos(@RequestParam int id, @RequestHeader(value = "Authorization") String authToken) {
+        return ResponseEntity.ok(service.delete(id, authToken));
     }
 
 }
--- backend/src/main/java/com/backend/erp/service/TodoServiceImpl.java
+++ backend/src/main/java/com/backend/erp/service/TodoServiceImpl.java
@@ -2,13 +2,15 @@ package com.backend.erp.service;
 
 import com.backend.erp.config.JwtService;
 import com.backend.erp.model.Todo;
-import com.backend.erp.model.User;
 import com.backend.erp.repository.TodoRepository;
 import com.backend.erp.repository.UserRepository;
 import com.backend.erp.request.TodoRequest;
 import com.backend.erp.response.SuccessResponse;
+import com.backend.erp.response.TodoResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,19 @@ public class TodoServiceImpl implements TodoService {
     private final UserRepository userRepository;
 
     private final JwtService jwtService;
+
+
+    public TodoResponse fetch(String authToken) {
+        authToken = authToken.substring(7);
+        var username = jwtService.extractUsername(authToken);
+        var user = userRepository.findByUsername(username).orElseThrow();
+        var todos = todoRepository.findByUserId(user.getId()).orElseThrow();
+
+        return TodoResponse.builder()
+                .todos(todos)
+                .build();
+
+    }
 
     public SuccessResponse add(TodoRequest request, String authToken) {
         authToken = authToken.substring(7);
@@ -35,5 +50,22 @@ public class TodoServiceImpl implements TodoService {
                 .message("Added")
                 .build();
 
+    }
+
+    public SuccessResponse delete(Integer id, String authToken) {
+        authToken = authToken.substring(7);
+        var username = jwtService.extractUsername(authToken);
+        var user = userRepository.findByUsername(username).orElseThrow();
+        var todo = todoRepository.findById(id).orElseThrow();
+        if (user.getId()== todo.getUser().getId()) {
+            todoRepository.deleteById(id);
+        }else{
+            return SuccessResponse.builder()
+                    .message("id = "+id+"user id = "+user.getId())
+                    .build();
+        }
+        return SuccessResponse.builder()
+                .message("Deleted successfully")
+                .build();
     }
 }
