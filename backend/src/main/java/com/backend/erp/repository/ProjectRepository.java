7fc5e81ac21d3fc38c6d1b487d6806f23b95db15
prakhar-5447 <sahuprakhar022003@gmail.com>
2023-04-18 15:47:22 +0530

    add new endpoints

--- backend/src/main/java/com/backend/erp/repository/ProjectRepository.java
+++ backend/src/main/java/com/backend/erp/repository/ProjectRepository.java
@@ -4,4 +4,5 @@ import com.backend.erp.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
 }
--- backend/src/main/java/com/backend/erp/service/ProjectService.java
+++ backend/src/main/java/com/backend/erp/service/ProjectService.java
@@ -1,15 +1,22 @@
 package com.backend.erp.service;
 
+import com.backend.erp.model.ProjectUserList;
 import com.backend.erp.request.ProjectRequest;
 import com.backend.erp.request.TaskRequest;
 import com.backend.erp.response.ProjectResponse;
 import com.backend.erp.response.SuccessResponse;
 import com.backend.erp.response.TaskResponse;
+import com.backend.erp.response.UserProjectResponse;
 
 public interface ProjectService {
     public SuccessResponse create(ProjectRequest request, String authToken);
 
     public ProjectResponse fetchProject(String authToken);
+
+    public UserProjectResponse fetchUser(String authToken, Integer id);
+
+    public SuccessResponse addUser(String authToken,Integer id,String user);
+
 
     public SuccessResponse assign(TaskRequest request, String authToken, Integer id);
 
